f182f049b40263483ead9a989d3f3f5ab7c8fef1
kiril <ksfilade@yahoo.com>
2019-11-14 10:40:18 +0100

    added cors policy

--- src/index.js
+++ src/index.js
@@ -12,7 +12,11 @@ app.use(fundriserRouter)
 //     next()
 // })
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.listen(port, ()=>{
     console.log('Server is on port'+port);
 })
