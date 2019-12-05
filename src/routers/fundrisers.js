b8863ac30c2b48fb8131dce5a771f9439b84efd7
kiril <ksfilade@yahoo.com>
2019-12-05 13:30:29 +0100

    test

--- src/routers/fundrisers.js
+++ src/routers/fundrisers.js
@@ -45,7 +45,7 @@ router.post('/fundrisers/donation', async (req, res) => {
 router.get('/fundrisers', async (req, res) => {
     console.log(req.query);
     Fundriser
-        .find(req.query.category ? { category: req.query.category } : void 0)
+        .find( { category: req.query.category } )
         .find(req.query.keyword ? { $text: { $search: req.query.keyword } } : void 0)
         .limit(parseInt(req.query.limit))
         .sort({ _id: -1 })
