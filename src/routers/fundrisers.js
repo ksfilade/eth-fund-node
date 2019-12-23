91e14df588087f725d7ac61e8f7fe6358a7d8f0f
Kiril <ksfilade@yahoo.com>
2019-12-23 10:43:36 +0100

    delted console logs

--- src/routers/fundrisers.js
+++ src/routers/fundrisers.js
@@ -89,14 +89,12 @@ router.put('/fundrisers/:id', adminAuth, async (req, res) => {
     })
 })
 router.get('/fundrisers/donations/:id', async (req, res) => {
-    console.log(req.params.id);
     let sum = 0;
-    Donation.aggregate([{ $match: { donationTo: req.params.id } },
-    { $group: { _id : req.params.id, sum : { $sum: "$amount" } } }]).then((res)=>{
-        console.log('aggregate');
-        console.log(res);
-        sum = res[0].sum;
-        console.log(sum);
+    Donation.aggregate([
+        { $match: { donationTo: req.params.id } },
+        { $group: { _id : req.params.id, sum : { $sum: "$amount" } } }])
+        .then((res)=>{
+            sum = res[0].sum;
     });
     Donation.find({ donationTo: req.params.id }).then(result => {
         res.send({result,sum})
